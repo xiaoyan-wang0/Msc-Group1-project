41c81d9aa0795631f46649cf45da8795428c2a67
Xiaoyan-Wang <d20124801@mytudublin.ie>
2021-10-02 15:34:39 +0100

    Ceate Vue3.0 basic environment. Include Ant design, vue-routerand vuex.

---
+++ group-project-frontend/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue';
+import router from './router'
+import store from './store'
+
+import 'ant-design-vue/dist/antd.css';
+
+
+const app = createApp(App);
+app.config.productionTip = false;
+app.use(store).use(router).use(Antd).mount('#app')
