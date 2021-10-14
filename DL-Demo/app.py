b99af44efe9d3e218d072bff201d28cfaadbacbb
Danielhua001 <91346473+Danielhua001@users.noreply.github.com>
2021-10-14 20:31:49 +0100

    Update app.py

--- DL-Demo/app.py
+++ DL-Demo/app.py
@@ -35,11 +35,6 @@ expose.secret_key = "super_secret_key"
 
 
 
-@expose.route('/', methods=['GET'])
-def index():
-    return jsonify("welcome to Arafa API")
-
-
 @expose.route('/api/tasks', methods=['GET'])
 def get_result():
     result = []
