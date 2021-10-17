4f712a263bbdec9b1dda0caf87a90c4bcc72512e
huguzhang <846629273@qq.com>
2021-10-17 18:48:09 +0100

    1.2

--- group-project-backend/database/movie.sql
+++ group-project-backend/database/movie.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 06/10/2021 13:09:25
+ Date: 17/10/2021 18:46:29
 */
 
 SET NAMES utf8mb4;
@@ -28,13 +28,35 @@ CREATE TABLE `user`  (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `createTime` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`userId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'ethan', 'd20124910@mytudublin.ie', '12345', '2021-10-05 05:34:16');
-INSERT INTO `user` VALUES (2, 'zhang', '12345@gmail.com', '123', '2021-10-05 07:19:39');
+INSERT INTO `user` VALUES (4, '+', '12345', 'dafd3b5a5240ff245e8249d9137525a2', '2021-10-11 17:40:54');
+INSERT INTO `user` VALUES (5, 'ethanzhang', 'ethan@gmail.com', 'dafd3b5a5240ff245e8249d9137525a2', '2021-10-12 12:05:44');
+
+-- ----------------------------
+-- Table structure for usercomments
+-- ----------------------------
+DROP TABLE IF EXISTS `usercomments`;
+CREATE TABLE `usercomments`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `userId` int(0) NULL DEFAULT NULL,
+  `tagId` int(0) NULL DEFAULT NULL,
+  `comment` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `createTime` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `movieId` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of usercomments
+-- ----------------------------
+INSERT INTO `usercomments` VALUES (1, 5, NULL, 'hello', '2021-10-15 08:16:36', 1);
+INSERT INTO `usercomments` VALUES (2, 5, NULL, 'hey', '2021-10-15 08:17:33', 1);
+INSERT INTO `usercomments` VALUES (3, 5, NULL, '12', '2021-10-15 08:18:49', 1);
+INSERT INTO `usercomments` VALUES (5, 5, NULL, 'good actor', '2021-10-15 08:38:11', 1);
 
 -- ----------------------------
 -- Table structure for userinfo
@@ -51,7 +73,7 @@ CREATE TABLE `userinfo`  (
   `constellation` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `movieTags` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`userInfoId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userinfo
