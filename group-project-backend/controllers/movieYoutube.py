164ceec816baf5897149c76478a90a7719f50673
Ege Bilir <egebilir@gmail.com>
2021-10-25 12:38:50 +0100

    Getting Youtube Data Interface added

---
+++ group-project-backend/controllers/movieYoutube.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint,render_template,request,make_response,jsonify,redirect,g
+from sqlalchemy import  text
+from application import app,db
+from common.libs.Helper import ops_renderJSON,ops_renderErrJSON,ops_render
+from common.libs.DataHelper import getCurrentTime
+from common.models.user import User
+from common.models.serializer import Serializer
+from common.libs.UserService import UserService
+import requests
+from common.libs.ToxicComments import do_pe,detector
+from jsonpath import jsonpath
+
+
+movie_page_Youtube = Blueprint( "movie_page_Youtube",__name__ )
+
+@movie_page_Youtube.route("/movieYoutubeReviews")
+def review():
+
+    # if 'current_user' in  g:
+    #     current_user = g.current_user
+    # if current_user == None : 
+    #     return ops_renderErrJSON( msg ="please login first")
+
+    # Time, user name, user image, comments
+
+    req = request.values
+    movieName = req['movieName'] 
+
+    response = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + str(movieName) + ' trailer' + '&type=video&key=AIzaSyCL_TmpbiATD9nisVU-TAbXCOZ6n1mu__E')
+    trailerId = jsonpath(response.json(),'$..videoId')
+
+    trailerIdToString = str(trailerId)[2:13]
+
+    response2 = requests.get('https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=' + trailerIdToString + '&key=AIzaSyCL_TmpbiATD9nisVU-TAbXCOZ6n1mu__E')
+    reviews = jsonpath(response2.json(),'$..textDisplay')
+    names = jsonpath(response2.json(),'$..authorDisplayName')
+    profileImages = jsonpath(response2.json(),'$..authorProfileImageUrl')
+    times = jsonpath(response2.json(),'$..publishedAt')
+    
+
+    list = []
+
+    for i in range(0, 20):
+        youtubeInfoDictionary = {
+        "username": names[i],
+        "review": reviews[i],
+        "profile picture": profileImages[i],
+        "time": times[i]
+        }
+        list.append(youtubeInfoDictionary)
+
+    return ops_renderJSON(msg = "Show Successfull!", data = list)
--- group-project-backend/www.py
+++ group-project-backend/www.py
@@ -18,12 +18,14 @@ from controllers.index import index_page
 from controllers.member import member_page
 from controllers.movieImdb import movie_page_Imdb
 from controllers.movieTmdb import movie_page_Tmdb
+from controllers.movieYoutube import movie_page_Youtube
 from controllers.comments import comments_page
 
 app.register_blueprint( index_page,url_prefix = "/" )
 app.register_blueprint( member_page,url_prefix = "/member" )
 app.register_blueprint( movie_page_Imdb,url_prefix = "/movieImdb" )
 app.register_blueprint( movie_page_Tmdb,url_prefix = "/movieTmdb" )
+app.register_blueprint( movie_page_Youtube,url_prefix = "/movieYoutube" )
 app.register_blueprint( comments_page,url_prefix = "/comments" )
 
 '''
