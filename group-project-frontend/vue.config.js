d7952de4a08f0e0f9a709643336d79cccc5c8489
Xiaoyan-Wang <d20124801@mytudublin.ie>
2021-10-28 22:21:42 +0100

    Update vue.config.js

--- group-project-frontend/vue.config.js
+++ group-project-frontend/vue.config.js
@@ -2,8 +2,8 @@ const webpack = require('webpack')
 module.exports = {
     devServer: {
         disableHostCheck: true,
-    
-        host: 'localhost',
+        host: 'http://3.10.181.15',
+        // host: 'localhost',
         port: 8080,
         //以上的ip和端口是我们本机的;下面为需要跨域的
         proxy: {//配置跨域
