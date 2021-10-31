78271eb7c696e929363bfccf2b22ca9d77bde4a0
huguzhang <846629273@qq.com>
2021-10-31 12:19:04 +0000

    2.0

--- group-project-backend/interceptors/Auth.py
+++ group-project-backend/interceptors/Auth.py
@@ -17,6 +17,9 @@ def before_request():
 @app.after_request
 def after_request( response ):
     app.logger.info("--------after_request--------")
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 '''
