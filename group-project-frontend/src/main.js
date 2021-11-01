9564794a46f91cf2bf0283e8e03070f21eb1c407
Xiaoyan-Wang <d20124801@mytudublin.ie>
2021-11-01 12:02:48 +0000

    Refine front

--- group-project-frontend/src/main.js
+++ group-project-frontend/src/main.js
@@ -9,14 +9,29 @@ import VueAxios from 'vue-axios'
 import 'element-plus/dist/index.css'
 import 'ant-design-vue/dist/antd.css';
 
+//fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+//fontawesome
+
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import "bootstrap"
 
 
 const app = createApp(App);
 app.config.productionTip = false;
 // axios.defaults.withCredentials=true;//让ajax携带cookie 
 app.use(VueAxios, axios)
-app.provide('axios', app.config.globalProperties.axios)  // provide 'axios'
+app.provide('axios', app.config.globalProperties.axios);  // provide 'axios'
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(store).use(router).use(Antd).use(ElementPlus).mount('#app')
