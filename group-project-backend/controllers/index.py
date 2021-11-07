d10259e417bfce49283e7ac91e105efe66b2e471
huguzhang <846629273@qq.com>
2021-11-07 17:24:17 +0000

    tmdb imdb cache and increasing the amount of concurrency

--- group-project-backend/controllers/index.py
+++ group-project-backend/controllers/index.py
@@ -51,7 +51,7 @@ def test():
 @index_page.route( "/json2" )
 def json_same():
     #str = '[We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products.With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may click to refuse to consent or access more detailed information and change your preferences before consenting.Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.]'
-    str2 = '[hello]'
+    str2 = ['hello']
     for i in range(1):
         result = detector(str2)
         senti = sentiment(str2)
