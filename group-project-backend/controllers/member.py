71179aed2640dceb903030fd66f4c47d546b4a26
huguzhang <846629273@qq.com>
2021-11-15 17:14:00 +0000

    user infomation

--- group-project-backend/controllers/member.py
+++ group-project-backend/controllers/member.py
@@ -262,12 +262,13 @@ def getRecommadationById():
     req = request.values
     userId = req['userId'] if "userId" in req else ""
     image = req['avatar'] if "avatar" in req else ""
-    app.logger.info( type(image) )
+    print(type(image) )
+    print(image)
     #
     img = base64.b64decode(str(image))
     #image_data = np.fromstring(img, np.uint8)
     #image_data = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-    app.logger.info( img )
+    print( img )
 
     db.session.query(Userinfo).filter(Userinfo.userId == userId).update({"image":str(img)})
     db.session.commit()
