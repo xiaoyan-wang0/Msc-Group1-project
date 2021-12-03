2d172c829d3b73d34f5de09d566e87e0870a89c5
huguzhang <846629273@qq.com>
2021-12-03 10:47:31 +0000

    ip

--- group-project-backend/controllers/comments.py
+++ group-project-backend/controllers/comments.py
@@ -108,8 +108,7 @@ def toxic():
     result['sentiment'] = senti['tag']
     return ops_renderJSON( msg = "comments detected successfully!",data = result )
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    print("dispute")
-    db.session.close()
-    db.engine.dispose()
+# @app.teardown_appcontext
+# def teardown_db(exception):
+#     db.session.close()
+#     db.engine.dispose()
