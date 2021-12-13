0952b4914989e770e538bee3088cfccac4e5d886
huguzhang <846629273@qq.com>
2021-12-13 00:58:24 +0000

    ip

--- group-project-backend/common/libs/ToxicComments.py
+++ group-project-backend/common/libs/ToxicComments.py
@@ -10,14 +10,11 @@ import numpy as np
 
 def do_pe(rs):
     pr=[]
-    with open('./tokenizer_4.pickle', 'rb') as f:
-        tokenizer = pickle.load(f)
     for r in rs:
         r = r.lower()
         pr.append(re.sub('[^a-zA-z0-9\s]','',r))
 
-    pr = tokenizer.texts_to_sequences(np.array(pr))
-    pr = pad_sequences(pr, maxlen=300)
+
 
     return pr
 
@@ -26,16 +23,16 @@ def detector(title):
     data = do_pe(title)
 
     input_data_json = json.dumps({"signature_name": "serving_default",
-                                        "instances": data.tolist()})
+                                        "instances": data})
 
     headers = {"content-type": "application/json"}          
 
     #response = requests.post('http://0.0.0.0:8501/v1/models/mymodel:predict', data=input_data_json,headers=headers)
     #response = requests.post('http://localhost:8501/v1/models/mymodel:predict', data=input_data_json,headers=headers)
-    response = requests.post('http://172.26.14.197:8501/v1/models/mymodel:predict', data=input_data_json,headers=headers)
+    response = requests.post('http://172.26.9.231:8501/v1/models/mymodel:predict', data=input_data_json,headers=headers)
 
     text1 =  title
 
-    label =  json.loads(response.text)['predictions'][0]
+    label =  json.loads(response.text)['predictions']
     result = {'title' : text1, 'tag' : label}
     return result
